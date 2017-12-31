2b0f518108292ccc3151e6aeb1b7e18432ed1ada
Yash Agrawal <yagrawl2@gmail.com>
2017-12-31 19:15:36 +0530

    resume create portal init

---
+++ js/script.js
@@ -0,0 +1,74 @@
+var resume;
+
+function getName() {
+    var firstname = document.getElementById('firstname').value;
+    var lastname = document.getElementById('lastname').value;
+    breakdown();
+    build('contact');
+}
+
+function getContact() {
+    alert('success so far');
+}
+
+function build(status){
+
+    var body = document.getElementsByTagName('body')[0];
+    var form = document.createElement('div');
+    var maintext = document.createElement('p');
+    var submit = document.createElement('input');
+
+    submit.className = 'submit-button';
+    submit.type = 'submit';
+    form.className = 'input-area';
+    maintext.className = 'input-text-main';
+
+    switch (status) {
+        case 'contact':
+            maintext.textContent = 'Contact Info';
+            form.appendChild(maintext);
+            var socialclass = [['fa fa-globe fa-2x', 'Website'],
+                               ['fa fa-user-o fa-2x', 'Email'],
+                               ['fa fa-github fa-2x', 'Github'],
+                               ['fa fa-phone-square fa-2x', 'Number'],
+                               ['fa fa-linkedin-square fa-2x', 'LinkedIn'],
+                               ['fa fa-location-arrow fa-2x', 'Location']];
+
+            socialclass.forEach(function(icon) {
+                var socialicon = document.createElement('i');
+                var input = document.createElement('input');
+                var br = document.createElement('br');
+
+                socialicon.className = icon[0];
+                input.className = 'text-bar input-text';
+                input.type = 'text';
+                input.placeholder = icon[1];
+                input.id = icon[1].toLowerCase();
+
+                form.appendChild(socialicon);
+                form.appendChild(input);
+                form.appendChild(br);
+
+            });
+
+            submit.value = 'Next';
+            submit.setAttribute('onclick','getContact()');
+
+            break;
+
+        default:
+            alert('Error');
+    }
+
+    var padding = document.createElement('p');
+
+    padding.appendChild(submit);
+    form.appendChild(padding);
+    body.appendChild(form);
+
+}
+
+function breakdown() {
+    var form = document.getElementById('input-area');
+    form.parentNode.removeChild(form);
+}
